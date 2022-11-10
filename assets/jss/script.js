18d2ca111ed6499b8f9ef3922f810393f1ddb7a7
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2022-11-10 15:03:54 -0600

    adding global variables

---
+++ assets/jss/script.js
@@ -0,0 +1,6 @@
+// Global Variables
+var arraySplit = ""; // Used to translate input array back to the user as a string with slice
+var exitApplication = false; // on/off switch for running the app
+var userInput = ""; // Important - will be used to generate password based on criteria
+var finalPassword = ""; // Important - used as a variable to store temporatly a random passoword.
+var displayPassword = ""; // Impoprtant- used to display the final password on the text area of the HTML element.
